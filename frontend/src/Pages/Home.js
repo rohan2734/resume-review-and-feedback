4ca44daff4b1075cc705e486f91bb0db5b962ccf
rohan2734 <jagandevaki1@gmail.com>
2022-10-10 13:28:28 +0530

    finished home page design

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import styles from "./Home.module.css";
 
 const Home = () => {
     return (
-        <div>
-            <h1>home</h1>
+        <div  className={styles.container__parent}>
+         <div className={styles.container}>
+            <h1 className={styles.container__title}>Getting a <span className={styles.container__title__highlight}>Great Job</span> starts with a <span className={styles.container__title__highlight}>Great Resume</span></h1>
+            <h2 className={styles.container__subtitle}>Get your resume reviewed by experts in various domains</h2>
+            <button className={styles.container__button}>Upload your resume for experts review</button>
         </div>
+        </div>
+       
     );
 }
 
