4b4b366365648d1cdef5c3acb51e0dbaff44d977
rohan2734 <jagandevaki1@gmail.com>
2022-11-02 21:02:49 +0530

    completed how it works part -home

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -2,6 +2,9 @@ import React from 'react';
 import styles from "./Home.module.css";
 
 import hw_create_resume from "../images/hw_create_resume.png";
+import hw_fill_resume from "../images/hw_fill_resume.png";
+import hw_upload from "../images/hw_upload.png";
+
 
 const Home = () => {
     return (
@@ -38,37 +41,41 @@ const Home = () => {
 
                 {/* step2 */}
                 <div className={styles.step_container}>
-                    <div className={styles.step_content}>
-                        <h2 className={styles.step_content__title}>create a resume</h2>
-                        <p className={styles.step_content__description}>
-                            From the home page, enter the name of the resume which 
-                            you want to create, and click Add
-                        </p>
+                     <div className={styles.image_container}>
+                        <img src={hw_create_resume} className={styles.step_image} />
                     </div>
+                   
                     
                     <div className={styles.step}>
                         <p className={styles.step__subtitle}>STEP</p>
-                        <p className={styles.step__count}>01</p>
+                        <p className={styles.step__count}>02</p>
                         <div className={styles.step__line}></div>
                     </div>
-                    {/* <img src={}/> */}
-                    <div className={styles.image_container}>
-                        <img src={hw_create_resume} className={styles.step_image} />
+
+                    <div className={styles.step_content}>
+                        <h2 className={styles.step_content__title}>Fill the details</h2>
+                        <p className={styles.step_content__description}>
+                        after creating the resume, fill the details about your intro, 
+                        technical skills, education , work experience, certificates, 
+                        name and address etc
+                        </p>
                     </div>
+                   
+                    
                 </div>
                 {/* step3 */}
                 <div className={styles.step_container}>
                     <div className={styles.step_content}>
-                        <h2 className={styles.step_content__title}>create a resume</h2>
+                        <h2 className={styles.step_content__title}>publish the resume for review</h2>
                         <p className={styles.step_content__description}>
-                            From the home page, enter the name of the resume which 
-                            you want to create, and click Add
+                        after filling the details, just click on save and publish the resume for review, now you can sit back and wait for the industrty expert to review your resume, or you can request a
+                        industry expert for review
                         </p>
                     </div>
                     
                     <div className={styles.step}>
                         <p className={styles.step__subtitle}>STEP</p>
-                        <p className={styles.step__count}>01</p>
+                        <p className={styles.step__count}>03</p>
                         <div className={styles.step__line}></div>
                     </div>
                     {/* <img src={}/> */}
